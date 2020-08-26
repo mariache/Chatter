c3073954502ffde705f5a9252053989497e16d52
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-26 15:41:22 +1200

    - mesages through ui

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,13 +1,42 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { Container } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+  return JSON.stringify(data);
+};
+
 const Chat = () => {
-  return <div>I'm a chat window</div>;
+  return (
+    <Container>
+      <Messages user="Jack" />
+    </Container>
+  );
 };
 
 export default () => (
