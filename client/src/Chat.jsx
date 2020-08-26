27adb20c58490a4f08adb877bbc714356587a6aa
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-26 23:00:55 +1200

    - form markup;

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -29,55 +29,93 @@ const Messages = ({ user }) => {
     return null;
   }
   return (
-    console.log(data),
-    (
-      <>
-        {data.messages.map(({ id, user: messageUser, content }) => (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: user === messageUser ? "flex-end" : "flex-start",
-              paddingBottom: "1em",
-            }}
-          >
-            {user !== messageUser && (
-              <div
-                style={{
-                  height: 50,
-                  width: 50,
-                  marginRight: "0.5em",
-                  border: "2px solid #e5e6ea",
-                  borderRadius: 25,
-                  textAlign: "center",
-                  fontSize: "18pt",
-                  paddingTop: 5,
-                }}
-              >
-                {messageUser.slice(0, 2).toUpperCase()}
-              </div>
-            )}
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          {user !== messageUser && (
             <div
               style={{
-                background: user === messageUser ? "blue" : "#e5e6ea",
-                color: user === messageUser ? "white" : "black",
-                padding: "1em",
-                borderRadius: "1em",
-                maxWidth: "60%",
+                height: 50,
+                width: 50,
+                marginRight: "0.5em",
+                border: "2px solid #e5e6ea",
+                borderRadius: 25,
+                textAlign: "center",
+                fontSize: "18pt",
+                paddingTop: 5,
               }}
             >
-              {content}
+              {messageUser.slice(0, 2).toUpperCase()}
             </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? "blue" : "#e5e6ea",
+              color: user === messageUser ? "white" : "black",
+              padding: "1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
           </div>
-        ))}
-      </>
-    )
+        </div>
+      ))}
+    </>
   );
 };
 
 const Chat = () => {
+  const [state, stateSet] = React.useState({
+    user: "Jack",
+    content: "",
+  });
+
   return (
     <Container>
-      <Messages user="Jacky" />
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label="User"
+            value={state.user}
+            onChange={(evt) =>
+              stateSet({
+                ...state,
+                user: evt.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label="Content"
+            value={state.content}
+            onChange={(evt) =>
+              stateSet({
+                ...state,
+                content: evt.target.value,
+              })
+            }
+            onKeyUp={(evt) => {
+              if (evt.keyCode === 13) {
+                onSend();
+              }
+            }}
+          />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()} style={{ width: "100%" }}>
+            Send
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 };
